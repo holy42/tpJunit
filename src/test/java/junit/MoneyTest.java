42506790bdba044cfa9f41e3734592c3c6db2eb2
holy42 <holymanarivo@gmail.com>
2025-08-17 19:56:43 +0300

    fourth question

--- src/test/java/junit/MoneyTest.java
+++ src/test/java/junit/MoneyTest.java
@@ -11,12 +11,23 @@ public class MoneyTest {
 		fail("Not yet implemented");
 	}
 	
+	@Test
 	public void testSimpleAdd() { 
 		Money m12CHF = new Money(12, "CHF"); // création de données 
 		Money m14CHF = new Money(14, "CHF"); 
 		Money expected = new Money(26, "CHF"); 
 		Money result = m12CHF.add(m14CHF); // exécution de la méthode testée 
 		assertTrue(expected.equals(result));
+	}
+	
+	@Test
+	public void testEquals() {
+	    Money m12CHF = new Money(12, "CHF");
+	    Money m14CHF = new Money(14, "CHF");
+	    assertTrue(!m12CHF.equals(null));
+	    assertEquals(m12CHF, m12CHF);
+	    assertEquals(m12CHF, new Money(12, "CHF"));
+	    assertTrue(!m12CHF.equals(m14CHF));
 	}
 
 }
