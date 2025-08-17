7a096d2ea5ac15f2793be20d0c15e2d4f7dfb6ac
holy42 <holymanarivo@gmail.com>
2025-08-17 20:14:47 +0300

    seventh question

--- src/test/java/junit/MoneyTest.java
+++ src/test/java/junit/MoneyTest.java
@@ -2,27 +2,32 @@ package junit;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class MoneyTest {
 	
-	@Test
-	public void testSimpleAdd() { 
-		Money m12CHF = new Money(12, "CHF"); // création de données 
-		Money m14CHF = new Money(14, "CHF"); 
-		Money expected = new Money(26, "CHF"); 
-		Money result = m12CHF.add(m14CHF); // exécution de la méthode testée 
-		assertTrue(expected.equals(result));
-	}
+	private Money f12CHF, f14CHF;
+
+    @Before
+    public void setUp() {
+        f12CHF = new Money(12, "CHF");
+        f14CHF = new Money(14, "CHF");
+    }
 	
-	@Test
-	public void testEquals() {
-	    Money m12CHF = new Money(12, "CHF");
-	    Money m14CHF = new Money(14, "CHF");
-	    assertTrue(!m12CHF.equals(null));
-	    assertEquals(m12CHF, m12CHF);
-	    assertEquals(m12CHF, new Money(12, "CHF"));
-	    assertTrue(!m12CHF.equals(m14CHF));
-	}
+    @Test
+    public void testSimpleAdd() {
+        Money expected = new Money(26, "CHF");
+        Money result = f12CHF.add(f14CHF);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testEquals() {
+        assertTrue(!f12CHF.equals(null));
+        assertEquals(f12CHF, f12CHF);
+        assertEquals(f12CHF, new Money(12, "CHF"));
+        assertTrue(!f12CHF.equals(f14CHF));
+    }
 
 }
