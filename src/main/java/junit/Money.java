d20cab9bc94e6da3228ab5e47fd561c47eaa2d6b
holy42 <holymanarivo@gmail.com>
2025-08-17 18:53:43 +0300

    first question

---
+++ src/main/java/junit/Money.java
@@ -0,0 +1,18 @@
+package junit; 
+	
+class Money { 
+	private int fAmount; 
+	private String fCurrency; 
+	
+	public Money(int amount, String currency) { 
+		fAmount = amount; 
+		fCurrency = currency; 
+	} 
+
+	public int amount() { 
+		return fAmount; 
+	} 
+	public String currency() { 
+		return fCurrency; 
+	} 
+} 
